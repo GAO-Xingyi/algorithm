ea56db4d6b169199426f5c2ea6e1447f0cc43953
Your Name <you@example.com>
2022-03-18 11:09:42 +0800

    ReverseList

---
+++ src/题库/链表反转/ReverseList.java
@@ -0,0 +1,21 @@
+package 题库.链表反转;
+
+import java.util.List;
+
+public class ReverseList {
+    // 创建内部类定义节点类型
+    static class ListNode {
+        int val;
+        ListNode next;
+        // 内部类构造方法书写构造计
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode recursion(ListNode head){
+
+        return null;
+    }
+}
