7a1ac8f3bee385fe4a11e0c0aae2c15b7ea6842a
Your Name <you@example.com>
2022-03-21 18:31:15 +0800

    SequenceList.md

--- src/Java数据结构与算法/List/SequenceList/SequenceList.java
+++ src/Java数据结构与算法/List/SequenceList/SequenceList.java
@@ -20,5 +20,55 @@ public class SequenceList<T> {
         // 初始化长度
         this.N = 0;
     }
+
+    public void clear(){
+        this.N = 0;
+    }
+
+    public boolean isEmpty() {
+        return N==0;
+    }
+
+    public int length(){
+        return N;
+    }
+
+    // 获取指定位置的元素
+    public T get(int i){
+        return eles[i];
+    }
+
+    // 向线性表中添加元素t
+    public void insert(T t){
+        eles[N++] = t;
+    }
+
+    // 在i元素处插入元素
+    public void insert(int i, T t){
+        N++;
+        for(int index=N-2; index > i; index--){
+            eles[index+1]=eles[index];
+        }
+        eles[i] = t;
+    }
+
+    public T remove(int i){
+        T current = eles[i];
+        for(int index=i;i<N-1;i++){
+            eles[index] = eles[index+1];
+        }
+        N--;
+        return current;
+    }
+
+    public int indexOF(T t){
+        for (int i=0;i<N;i++){
+            if(eles[i].equals(t)){
+                return 1;
+            }
+            }
+        return -1;
+        }
 }
+
 
